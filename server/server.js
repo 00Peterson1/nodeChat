73ec793f58db97ddbfea1baf4d571c8c7e021ac3
00Peterson1 <161578371+00Peterson1@users.noreply.github.com>
2026-05-27 00:32:36 +0300

    Feat: Added the Client Side Folder and file and modified the server file

--- server/server.js
+++ server/server.js
@@ -5,12 +5,29 @@ const port = 4000;
 const server = http.createServer((req, res)=> {
     console.log(`--New Method & Url-- ${req.method} ${req.url}`);
 
+    if(req.method === "GET" &&req.url === "/"){
+        const filePath = path.join(__dirname, "../client/index.html");
 
-    res.writeHead(200,
-        {"content-type": "text/plain"}
-    );
-    res.write("Chat server running");
-    res.end();
+        fs.readFile(filePath , (err, data)=> {
+            if(err) {
+                   res.writeHead(500, {"content-type": "text/plain"});
+                   
+                console.log(err)
+                return res.end("Internal Server Error");
+            }
+
+            res.writeHead(200, {
+                "content-type": "text/plain"
+            })
+            res.write(data);
+            res.end();
+        })
+    } else {
+        res.writeHead(404, {
+            "content-type": "text/plain"
+        })
+        res.end("Page Not Found");
+    }
 })
 
 server.listen(port, ()=>{
